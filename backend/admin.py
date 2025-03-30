aeb4f71dff3ddee28adc0c376f5127c2759f3e58
Jay <zhuravlevajuliia@gmail.com>
2025-03-30 23:45:14 +0400

    Добавлены модели для системы автоматизации закупок

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import (
+    User, Shop, Category, Product, ProductInfo,
+    Parameter, ProductParameter, Contact,
+    Order, OrderItem, ConfirmEmailToken
+)
 
-# Register your models here.
+# Регистрация моделей в админке
+admin.site.register(User)
+admin.site.register(Shop)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductInfo)
+admin.site.register(Parameter)
+admin.site.register(ProductParameter)
+admin.site.register(Contact)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ConfirmEmailToken)
