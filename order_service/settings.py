94b047fa8de04ec53e32f802b644d371df20f8d1
Julia Zhuravleva <zhuravlevajuliia@gmail.com>
2025-05-10 21:56:27 +0400

    Исправлен тестовый воркфлоу с установкой DJANGO_SETTINGS_MODULE, без использования ||, и с fallback переменными внутри settings.py

--- order_service/settings.py
+++ order_service/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-ci-testing-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
